de93d45c0d3ceef378b64d9af77c38dc75c1b21b
David Fisher <fisherds@rose-hulman.edu>
2015-01-04 01:04:01 -0500

    Objective-C Interface Builder app questions
    2 down, 2 to go

--- courses/ios-swift-intro/assets/js/activity-5.1.js
+++ courses/ios-swift-intro/assets/js/activity-5.1.js
@@ -1,23 +1,67 @@
 var activity = [
 
-    "<b>1.</b> ?<br>",
+    "<b>1.</b> As of 2015 how much iOS code has been written in Objective-C vs Swift?  How many companies use Objective-C compared to Swift?<br>",
 	{
 		questionType : "multiple choice",
 		choices : [
-				["", false, "Please try again." ],
-				["", false, "Please try again." ],
-				["", true, "Correct!" ],
-				["", false, "Please try again." ] ]
+				["Objective-C first appeared in 1983 (<a target='_blank' href='http://en.wikipedia.org/wiki/Objective-C'>link</a>).  Swift was released in 2014.  So almost all companies doing iOS development have Objective-C code (even if they plan to move in part) and almost all apps you use are made in Objective-C", true, "Correct!  Swift is clearly better, but the transition will take time." ],
+				["I don't know they are probably both pretty even in usage???", false, "Please try again." ] ]
 	},
 
-    "<br><br><b>2.</b> ?<br>",
+    "<br><br><b>2.</b> In order to teach Objective-C we don't want to remake ALL the Storyboard stuff.  So our approach is what?<br>",
 	{
 		questionType : "multiple choice",
 		choices : [
-				["", false, "Please try again." ],
-				["", false, "Please try again." ],
-				["", true, "Correct!" ],
-				["", false, "Please try again." ] ]
+				["Copy the Storyboard file from a Swift project into an Objective-C project.", false, "Please try again." ],
+				["Remake the app from scratch in Objective-C", false, "Please try again." ],
+				["Make a copy of the Swift project. Break the Swift Storyboard connections and remake all the code and connections to Objective-C.", true, "Correct!" ],
+				["Too soon to tell.", false, "Please try again." ] ]
 	},
 
+
+    "<br><br><b>3.</b> Mark the code snippets below as Swift, Objective-C, or Neither?<br>",
+
+	{
+		questionType : "multiple choice group",
+		questionsList : [
+				{
+					questionHTML : "<b>a.</b> @property (weak, nonatomic) IBOutlet UISlider* redSlider;",
+					choices : [ "Swift", "Objective-C", "Neither" ],
+					correctIndex : 1
+				},
+				{
+					questionHTML : "<b>b.</b> @IBOutlet weak var redSlider: UISlider!",
+					choices : [ "Swift", "Objective-C", "Neither" ],
+					correctIndex : 0
+				},
+				{
+					questionHTML : "<b>c.</b> @IBAction func sliderChanged(sender: AnyObject) { ... }",
+					choices : [ "Swift", "Objective-C", "Neither" ],
+					correctIndex : 0
+				}, {
+					questionHTML : "<b>d.</b> - (IBAction) sliderChanged:(id) sender;",
+					choices : [ "Swift", "Objective-C", "Neither" ],
+					correctIndex : 1
+				} ],
+		allCorrectOutput : "Well done!  Neat to think about the language differences",
+		someIncorrectOutput : "Please try again. Hints: 2 are Swift and 2 are Objective-C (non are Neither)",
+	},
+
+    "<br><br><b>4.</b> What are some things you've observed about Objective-C already?",
+
+	{
+		questionType : "multiple choice group",
+		questionsList : [
+				{
+					questionHTML : "",
+					choices : [ "Unlike Java, Python, or Swift, it uses a * to show some something is an Object reference (like C did). Hint UISlider* redSlider",
+					            "Functions have a return value on the left (like every non-Swift language). Hint IBAction as the return values is the same as void",
+					            "Functions use the keyword func (just like Swift)",
+					            "Tools like Interface Builder or the Cocoa Touch framework are used VERY differently than in Swift.",
+					            "Objective-C uses two files a .h and a .m to separate the public interface from the implementation (like other C based languages)"],
+					correctIndex : [0, 1, 4]
+				} ],
+		allCorrectOutput : "Well done!  Neat to think about the language differences",
+		someIncorrectOutput : "Please try again. Hints: 3 answers are correct.  Objective-C doesn't use func and the tools work pretty much the same.",
+	},
 ];
--- courses/ios-swift-intro/assets/js/activity-5.2.js
+++ courses/ios-swift-intro/assets/js/activity-5.2.js
@@ -1,23 +1,63 @@
 var activity = [
 
-    "<b>1.</b> ?<br>",
+                "<b>Swift code</b> snippet from the OLD app<br>",
+
+
+        		"<img style='width: 100%;' src='assets/img/swift_color_sliders_code.png' alt='Swift Color sliders code'><br>",
+
+                "<br><br><b>Objective-C code</b> snippet from the NEW app<br>",
+
+
+        		"<img style='width: 90%;' src='assets/img/objc_color_sliders_code.png' alt='Objective-C Color sliders code'><br>",
+    "<br><br><b>1.</b> Based on what you know about Swift, Objective-C, and looking at the code above answer the questions below?<br>",
+
+	{
+		questionType : "multiple choice group",
+		questionsList : [
+				{
+					questionHTML : "<b>a.</b> Makes you type the keyword <b>self</b> a lot more often",
+					choices : [ "Swift", "Objective-C", "Both", "Neither" ],
+					correctIndex : 1
+				},
+				{
+					questionHTML : "<b>b.</b> Uses the keyword <b>self</b> in a class to reference the instance variable that the method was called on",
+					choices : [ "Swift", "Objective-C", "Both", "Neither" ],
+					correctIndex : 2
+				},
+				{
+					questionHTML : "<b>c.</b> <b>Requires</b> you to use ; to end a line of code",
+					choices : [ "Swift", "Objective-C", "Both", "Neither" ],
+					correctIndex : 1
+				}, {
+					questionHTML : "<b>d.</b> Uses a dot notation with parentheses at the end to call a function, similar to most other languages (although self. can be omitted in the call like above)",
+					choices : [ "Swift", "Objective-C", "Both", "Neither" ],
+					correctIndex : 0
+				}, {
+					questionHTML : "<b>e.</b> Puts the return value to the left in a function signature line, similar to most other languages (void if no return value exist)",
+					choices : [ "Swift", "Objective-C", "Both", "Neither" ],
+					correctIndex : 1
+				}, {
+					questionHTML : "<b>f.</b> Uses [ ] to create objects and call functions",
+					choices : [ "Swift", "Objective-C", "Both", "Neither" ],
+					correctIndex : 1
+				}, {
+					questionHTML : "<b>g.</b> Has a concept for External parameter names (green, blue, alpha)",
+					choices : [ "Swift", "Objective-C", "Both", "Neither" ],
+					correctIndex : 2
+				}, {
+					questionHTML : "<b>h.</b> Requires you to ALWAYS use words like green:, blue:, alpha: when calling a function with multiple parameters passed in",
+					choices : [ "Swift", "Objective-C", "Both", "Neither" ],
+					correctIndex : 1
+				} ],
+		allCorrectOutput : "Well done!  Neat to think about the language differences",
+		someIncorrectOutput : "Please try again. Hints: 1 Swift, 5 Objective-C, 2 Both, 0 Neither.",
+	},
+
+    "<br><br><b>2.</b> Did you complete the Objecive-C version of Color Sliders (and submit it on Moodle if taking this course for credit)?<br>",
 	{
 		questionType : "multiple choice",
 		choices : [
-				["", false, "Please try again." ],
-				["", false, "Please try again." ],
-				["", true, "Correct!" ],
-				["", false, "Please try again." ] ]
+				["Yep.  All set!", true, "Great!" ],
+				["No so much.", false, "Well ya kinda need to get that done.  Take a break and work on it tomorrow." ] ]
 	},
-
-    "<br><br><b>2.</b> ?<br>",
-	{
-		questionType : "multiple choice",
-		choices : [
-				["", false, "Please try again." ],
-				["", false, "Please try again." ],
-				["", true, "Correct!" ],
-				["", false, "Please try again." ] ]
-	},
-
 ];
