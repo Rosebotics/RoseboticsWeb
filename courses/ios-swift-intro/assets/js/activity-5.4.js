6e7756f85e65efc2fa72a2c59294371eedd78d1f
David Fisher <fisherds@rose-hulman.edu>
2015-01-04 00:03:09 -0500

    Placeholders for the first Objective-C Unit

---
+++ courses/ios-swift-intro/assets/js/activity-5.4.js
@@ -0,0 +1,11 @@
+var activity = [
+
+    "<b>1.</b> Did you complete the Lights Out Universal App lab (and submit it on Moodle if taking this course for credit)?<br>",
+	{
+		questionType : "multiple choice",
+		choices : [
+				["Yep.  All set!", true, "Great!" ],
+				["No so much.", false, "Well ya kinda need to get that done.  Take a break and work on it tomorrow." ] ]
+	},
+
+];
