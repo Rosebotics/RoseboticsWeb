217b9ffd7bbd3c18723e0b2b2f1d6c686870574a
David Fisher <fisherds@rose-hulman.edu>
2015-02-12 15:32:41 -0500

    Adding instructions to NOT add hostname (only to iOS)
    Still need to add this to Android

--- courses/ios-core-data/assets/js/activity-1.3.js
+++ courses/ios-core-data/assets/js/activity-1.3.js
@@ -1,23 +1,49 @@
 var activity = [
 
-    "<b>1.</b> ?<br>",
+    "<b>1.</b> You regularly use an Array with a UITableView.  When we used an array of NSString objects what was our UITableViewStyle in our Storyboard prototype?<br>",
 	{
 		questionType : "multiple choice",
 		choices : [
-				["", false, "Please try again." ],
-				["", false, "Please try again." ],
-				["", true, "Correct!" ],
-				["", false, "Please try again." ] ]
+				["Custom", false, "Please try again." ],
+				["Basic", true, "Correct!  It shows only a single UILabel in the row." ],
+				["Right Detail", false, "Please try again." ],
+				["Left Detail", false, "Please try again." ],
+				["Subtitle", false, "Please try again." ] ]
 	},
 
-    "<br><br><b>2.</b> ?<br>",
+    "<br><br><b>2.</b> When we used an array of MovieQuote objects what was our UITableViewStyle in our Storyboard prototype?<br>",
 	{
 		questionType : "multiple choice",
 		choices : [
-				["", false, "Please try again." ],
-				["", false, "Please try again." ],
-				["", true, "Correct!" ],
-				["", false, "Please try again." ] ]
+				["Custom", false, "Please try again." ],
+				["Basic", false, "Please try again." ],
+				["Right Detail", false, "Please try again." ],
+				["Left Detail", false, "Please try again." ],
+				["Subtitle", true, "Correct!  That one is my favorite for showing two labels in this case." ] ]
 	},
+
+
+    "<br><br><b>3.</b> If you wanted to show <b>more</b> than 2 UILabels what type of UITableViewStyle would you use?<br>",
+	{
+		questionType : "multiple choice",
+		choices : [
+				["Custom", true, "Correct! Maybe we'll do that in the next unit. :)" ],
+				["Basic", false, "Please try again." ],
+				["Right Detail", false, "Please try again." ],
+				["Left Detail", false, "Please try again." ],
+				["Subtitle", false, "Please try again." ] ]
+	},
+
+    "<br><br><b>4.</b> In the previous lecture we implemented two <b>dataSource</b> methods for the UITableView.  In this lecture we implemented our first <b>delegate</b> method for the UITableView.  What <b>delegate</b> method did we implement to listen for users clicking a table cell?<br>",
+	{
+		questionType : "multiple choice",
+		choices : [
+				["canEditRowAtIndexPath", false, "Please try again." ],
+				["cellForRowAtIndexPath", false, "Please try again." ],
+				["didSelectRowAtIndexPath", true, "Correct!"],
+				["numberOfRowsInSection", false, "Please try again."  ],
+				["numberOfSectionsInTableView", false, "Please try again." ] ]
+	},
+
 
 ];
