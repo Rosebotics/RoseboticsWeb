13c367048e0bb5ef036013f77beb59e94f1fbffc
David Fisher <fisherds@rose-hulman.edu>
2015-03-12 13:11:44 -0400

    missed a file in last commit

---
+++ handlers/course_handlers.py
@@ -0,0 +1,35 @@
+import logging
+
+from handlers import base_handler
+from utils import progress_utils
+
+
+### PAGES ###
+class WebCoursePage(base_handler.BasePage):
+  def template_file(self):
+    return "templates/web_course.html"
+
+  def page_title(self):
+    return "Web Development"
+
+  def update_values(self, user, values):
+    """ Updates the values passed to Jinja to add the progress in each track and the overall course """
+    values["progress"] = progress_utils.get_progress_for_course(user, "web")
+
+  def requires_oauth(self):
+    return True
+
+
+class IosCoursePage(base_handler.BasePage):
+  def template_file(self):
+    return "templates/ios_course.html"
+
+  def page_title(self):
+    return "iOS Development"
+
+  def update_values(self, user, values):
+    """ Updates the values passed to Jinja to add the progress in each track and the overall course """
+    values["progress"] = progress_utils.get_progress_for_course(user, "ios")
+
+  def requires_oauth(self):
+    return True
