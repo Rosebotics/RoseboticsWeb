c63060ddc0230159e281c6b037e80aab30cdfc4e
David Fisher <fisherds@rose-hulman.edu>
2015-03-27 13:07:26 -0400

    Added an email field to the profile

--- handlers/main_handler.py
+++ handlers/main_handler.py
@@ -16,7 +16,7 @@ class CoursesPage(base_handler.BasePage):
     return "Courses"
 
   def update_values(self, user, values):
-    values['active_page'] = 'courses'
+    values["active_page"] = "courses"
     if not user:
       return
     # TODO: get progress for all courses
@@ -25,26 +25,27 @@ class CoursesPage(base_handler.BasePage):
 
 class CompetitionPage(base_handler.BasePage):
   def template_file(self):
-    return 'templates/underconstruction.html'
+    return "templates/underconstruction.html"
 
   def page_title(self):
-    return 'Competition'
+    return "Competition"
 
 
 class PlatformPage(base_handler.BasePage):
   def template_file(self):
-    return 'templates/underconstruction.html'
+    return "templates/underconstruction.html"
 
   def page_title(self):
-    return 'Platform'
+    return "Platform"
 
 
 ### PAGES ###
 class EditProfileAction(base_handler.BaseAction):
-  
+
   def handle_post(self, rosebotics_student):
-    rosebotics_student.name = self.request.get('name')
-    rosebotics_student.nickname = self.request.get('nickname')
-    rosebotics_student.username = self.request.get('username')
-    rosebotics_student.details = self.request.get('connection')
+    rosebotics_student.name = self.request.get("name")
+    rosebotics_student.nickname = self.request.get("nickname")
+    rosebotics_student.alt_email = self.request.get("alt_email")
+    rosebotics_student.username = self.request.get("username")
+    rosebotics_student.details = self.request.get("connection")
     rosebotics_student.put()
--- models/rosebotics_models.py
+++ models/rosebotics_models.py
@@ -3,8 +3,9 @@ from google.appengine.ext import ndb
 
 class RoseboticsStudent(ndb.Model):
   name = ndb.StringProperty()
-  username = ndb.StringProperty()
   nickname = ndb.StringProperty()
+  alt_email = ndb.StringProperty()
+  username = ndb.StringProperty()
   details = ndb.TextProperty()
 
 class RecentTrack(ndb.Model):
