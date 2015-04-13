0eef77eb4c4b193bf79ae033294f0b679a49cd2c
rockwotj <rockwotj@rose-hulman.edu>
2015-04-13 00:19:18 -0400

    Fixed navbar bootstrap elements

--- appengine/static/teams/js/app.js
+++ appengine/static/teams/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('TeamApp', [ 'TeamControllers', 'TeamServices' ])
+angular.module('TeamApp', [ 'TeamControllers', 'ModalControllers', 'TeamServices', 'ui.bootstrap' ])
 .run(function() {
   angular.element(document.querySelectorAll(".hidden.container")).removeClass("hidden");
   angular.element(document.querySelector("div.spinner")).addClass("hidden");
--- appengine/static/teams/js/controllers.js
+++ appengine/static/teams/js/controllers.js
@@ -2,3 +2,27 @@ angular.module('TeamControllers', [])
 .controller('MainCtrl', function() {
   this.greeting = "Hello World!";
 });
+
+angular.module('ModalControllers', [])
+.controller('ModalToggleCtrl', function($modal) {
+	this.toggleSignOutModal = function() {
+		var modalInstance = $modal.open({
+		  templateUrl: 'logout_modal.html',
+		  controller: 'SimpleModalInstanceCtrl',
+		  controllerAs: 'modal'
+		});
+	};
+	this.toggleEditProfileModal = function() {
+		var modalInstance = $modal.open({
+		  templateUrl: 'edit_profile_modal.html',
+		  controller: 'SimpleModalInstanceCtrl',
+		  controllerAs: 'modal'
+		});
+	};
+})
+.controller('SimpleModalInstanceCtrl', function ($modalInstance) {
+  this.cancel = function () {
+    $modalInstance.close('cancel');
+  };
+});
+
