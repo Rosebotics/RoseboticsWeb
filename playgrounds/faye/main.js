3be861ad880fbd8406e5c3c15e06844f3e820aad
Faye <lis@rose-hulman.edu>
2015-04-19 16:14:29 -0400

    Logo work
    added new apple logo, changes to android eyes

--- playgrounds/faye/main.js
+++ playgrounds/faye/main.js
@@ -1,23 +1,33 @@
-window.onload = function onLoad() {
-    var circle = new ProgressBar.Circle('#example-percent-container', {
-    color: '#FCB03C',
-    strokeWidth: 3,
-    trailWidth: 1,
-    duration: 1500,
-    text: {
-        value: '0'
-    },
-    step: function(state, bar) {
-        bar.setText((bar.value() * 100).toFixed(0));
-    }
+$(document).ready(function(){
+    
+    $("#android-course").hover(
+
+        function(){
+            console.log("mouse in");
+            var circle = new ProgressBar.Circle('#android-course', {
+            color: '#FCB03C',
+            strokeWidth: 3,
+            trailWidth: 1,
+            duration: 1500,
+            text: {
+                value: '0'
+            },
+            step: function(state, bar) {
+            bar.setText((bar.value() * 100).toFixed(0));
+            }
+            });
+
+            circle.animate(1, function() {
+            circle.animate(0);
+            })
+        },
+
+        function(){
+            console.log("mouse out");
+        }
+
+    );
 });
-
-
-    circle.animate(1, function() {
-    circle.animate(0);
-});
-
-};
 
 
 
