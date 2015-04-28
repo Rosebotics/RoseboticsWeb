07d9c66c5a9132e8c7a66afed6ed7fd849e02588
Faye <lis@rose-hulman.edu>
2015-04-28 02:20:29 -0400

    Progress Circle Overlay Animation On Hover!!

--- playgrounds/faye/main.js
+++ playgrounds/faye/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
 
+    var circle = new ProgressBar.Circle('#android-course-graphics', {
+            color: '#FCB03C',
+            strokeWidth: 3,
+            trailWidth: 1,
+            duration: 1500,
+           
+            });
+
+    
+
     $('#resume-btn').hover(
         function(){
             console.log('resume mouse in');
@@ -13,30 +23,16 @@ $(document).ready(function(){
 
     $('#video-btn').magnificPopup({type:'iframe'});
     
-    $("#android-course-img").hover(
+    $("#android-course-graphics").hover(
 
         function(){
             console.log("mouse in");
-            var circle = new ProgressBar.Circle('#android-course-progress', {
-            color: '#FCB03C',
-            strokeWidth: 3,
-            trailWidth: 1,
-            duration: 1500,
-            text: {
-                value: '0'
-            },
-            step: function(state, bar) {
-            bar.setText((bar.value() * 100).toFixed(0));
-            }
-            });
-
-            circle.animate(1, function() {
-            circle.animate(0);
-            })
+            circle.animate(0.6, {easing: "easeInOut"})
         },
 
         function(){
             console.log("mouse out");
+            circle.animate(0, {easing: "easeInOut"})
         }
 
     );
