9987f5e13a6b769d6bcf99cb647a256fe2bfa9a9
rockwotj <rockwotj@rose-hulman.edu>
2015-05-11 21:25:17 -0400

    Changed Track Progress Bars to be a variable amount

--- appengine/static/js/rosebotics.js
+++ appengine/static/js/rosebotics.js
@@ -22,6 +22,7 @@ var googleyellow = '#f4b400';
 
 $(document).ready(function(){
 	loginModalFunctions();
+	
 	$('#resume-btn').hover(
         function(){
             //console.log('resume mouse in');
@@ -35,14 +36,6 @@ $(document).ready(function(){
 
 
 	$('#resume-btn2').hover(
-        function(){
-            //console.log('resume mouse in');
-            $('#resume-icon2').removeClass('glyphicon-pause').addClass('glyphicon-play');
-        },
-        function(){
-            //console.log('resume mouse out'); 
-            $('#resume-icon2').removeClass('glyphicon-play').addClass('glyphicon-pause');
-        }
     );
 
     $('.list-group-item').hover(
@@ -56,33 +49,7 @@ $(document).ready(function(){
             
         }
     );
-
-	var line1 = new ProgressBar.Line('#track-progress-container1', {
-    color: progresscolor,
-    strokeWidth: 3,
-    step: function(state, line1) {
-        line1.path.setAttribute('stroke', state.color);
-    }
-
-	});
-
-	var line2 = new ProgressBar.Line('#track-progress-container2', {
-    color: progresscolor,
-    strokeWidth: 3,
-    step: function(state, line2) {
-        line2.path.setAttribute('stroke', state.color);
-    }
-
-	});
-
-	var line3 = new ProgressBar.Line('#track-progress-container3', {
-    color: progresscolor,
-    strokeWidth: 3,
-    step: function(state, line3) {
-        line3.path.setAttribute('stroke', state.color);
-    }
-	});
-
+   
 	var circle = new ProgressBar.Circle('#course-progress-container', {
     trailColor: '#f1f2f2',
     trailWidth: 1,
@@ -90,15 +57,12 @@ $(document).ready(function(){
     strokeWidth: 3,
     trailWidth: 1,
     duration: 1500,
-
     step: function(state, circle) {
         circle.path.setAttribute('stroke', state.color);
         //console.log((circle.value() * 100).toFixed(0));
         $('#current-progress-percentage').text((circle.value() * 100).toFixed(0) + "%");
     }
 	});
-
-
 
 	circle.animate(0.5, {
         easing: "easeIn",
@@ -111,20 +75,25 @@ $(document).ready(function(){
         to: {color: progresscolor}
     });
     });  // Number from 0.0 to 1.0
+	for(var i = 0; i < currentCourseTrackPercentages.length; i++) {
+			var line = new ProgressBar.Line('#track-progress-container' + (i + 1), {
+				color : progresscolor,
+				strokeWidth : 3,
+				step : function(state, line1) {
+					line1.path.setAttribute('stroke', state.color);
+				}
 
-	line1.animate(currentCourseTrackPercentages[0], {
-    from: {color: googlered},
-    to: {color: progresscolor}
-    });  // Number from 0.0 to 1.0
-    // Number from 0.0 to 1.0
-	line2.animate(currentCourseTrackPercentages[1], {
-    from: {color: googlered},
-    to: {color: progresscolor}
-    });  // Number from 0.0 to 1.0
-      // Number from 0.0 to 1.0
-	line3.animate(currentCourseTrackPercentages[2], {
-    from: {color: googlered},
-    to: {color: progresscolor}
-    });  // Number from 0.0 to 1.0
-     // Number from 0.0 to 1.0
+			});
+			line.animate(currentCourseTrackPercentages[i], {
+				from : {
+					color : googlered
+				},
+				to : {
+					color : progresscolor
+				}
+			});	
+	}
+			
+	
+
 });
