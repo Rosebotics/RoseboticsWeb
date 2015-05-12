fc0a8c3e46991482fa42ab490c6c3adf90b225f1
Faye <lis@rose-hulman.edu>
2015-05-12 03:08:18 -0400

    Responsive web course page with intro video

--- playgrounds/faye/test3.js
+++ playgrounds/faye/test3.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
 	$('#video-btn').click(function(){
 		$('#video-container').fadeIn(1000).removeClass('hidden');
 	});
+
+	$('.list-group-item').click(function(){
+		$(this).find("span").toggleClass("glyphicon-chevron-down").toggleClass("glyphicon-chevron-right");
+	});
 });
