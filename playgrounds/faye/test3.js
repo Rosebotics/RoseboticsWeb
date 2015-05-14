55c6053e51ed2635f0f7599f0ab1a7e988b89813
Faye <lis@rose-hulman.edu>
2015-05-14 02:51:30 -0400

    Video Theater mode

--- playgrounds/faye/test3.js
+++ playgrounds/faye/test3.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('#video-btn').click(function(){
-		$('#video-container').fadeIn(1000).removeClass('hidden');
+		$('#theater').fadeIn(1000).removeClass('hidden');
 	});
 
 	$('.list-group-item').click(function(){
