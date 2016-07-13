b56b5b539429272118af4ae46e8f2d473f46806e
David Fisher <fisherds@rose-hulman.edu>
2016-07-13 10:19:52 -0400

    questions and above the video content for the Intro to firebase iOS unit lessons 13-15

--- appengine/courses/ios-firebase/assets/js/activity-1.13.js
+++ appengine/courses/ios-firebase/assets/js/activity-1.13.js
@@ -1,23 +1,38 @@
 var activity = [
 
-    "<b>1.</b> ?<br>",
-	{
-		questionType : "multiple choice",
-		choices : [
-				["", false, "Please try again." ],
-				["", false, "Please try again." ],
-				["", true, "Correct!" ],
-				["", false, "Please try again." ] ]
-	},
+    "<b>1.</b>Which option below would make a FIRDatabaseReference (a pointer to a Firebase Database node) to our <b>quotes</b> path?<br>",
+  {
+    questionType : "multiple choice",
+    choices : [
+        ["FIRDatabase.database().reference()", false, "Please try again." ],
+        ["FIRApp.database().reference().child(\"quotes\")", false, "Please try again." ],
+        ["Firebase.reference().child(\"quotes\")", false, "Please try again." ],
+        ["FIRDatabase.database().reference().child(\"quotes\")", true, "Correct!" ] ]
+  },
 
-    "<br><br><b>2.</b> ?<br>",
-	{
-		questionType : "multiple choice",
-		choices : [
-				["", false, "Please try again." ],
-				["", false, "Please try again." ],
-				["", true, "Correct!" ],
-				["", false, "Please try again." ] ]
-	},
+    "<br><br><b>2.</b> Could we have used a single .Value event observer on the quotes path to retrieve all quotes instead of using 3 different observers?<br>",
+  {
+    questionType : "multiple choice",
+    choices : [
+        ["Yes.  It'd work. It's just not recommended when working with the repeated object pattern. So it works but there is a better way.", true, "Correct!" ],
+        ["Can't use a .Value observer.  You must use .ChildAdded .ChildChanged and .ChildRemoved.", false, "Please try again." ] ]
+  },
+
+  "<br><br><b>3.</b> For this app could we have just added the 3 event observers to viewDidLoad and never remove the observers?<br>",
+  {
+    questionType : "multiple choice",
+    choices : [
+        ["Sure. In this app they really don't need to be removed. Only removing them to show best practice for bigger apps.", true, "Correct!" ],
+        ["When you add a continuous observer you are required to remove it, otherwise it will eventually cause your app to crash.", false, "Please try again." ] ]
+  },
+
+  "<br><br><b>4.</b> We are working through the R in CRUD.  What is the current status of our C, U, and D from this app to Firebase?<br>",
+  {
+  questionType : "multiple choice",
+  choices : [
+      ["They still work.", false, "Please try again." ],
+      ["We haven't even started them.  We're only half way through R.", true, "Correct!" ] ]
+  },
+
 
 ];
--- appengine/courses/ios-firebase/assets/js/activity-1.14.js
+++ appengine/courses/ios-firebase/assets/js/activity-1.14.js
@@ -1,23 +1,20 @@
 var activity = [
 
-    "<b>1.</b> ?<br>",
-	{
-		questionType : "multiple choice",
-		choices : [
-				["", false, "Please try again." ],
-				["", false, "Please try again." ],
-				["", true, "Correct!" ],
-				["", false, "Please try again." ] ]
-	},
+    "<b>1.</b> If you delete the last quote from Firebase what happens?<br>",
+  {
+    questionType : "multiple choice",
+    choices : [
+        ["The quotes path is completely removed (Firebase automatically cleans it up), but that's no biggy.  It'll get added back whenever a quote gets added.", true, "Correct!" ],
+        ["The quotes path is completely removed, and that's a bit of a pain since you need to make sure to manually add it back before you can add any more quotes.", false, "Please try again." ],
+        ["The quotes path is still present.  There are simply no quotes on the path.  No biggy", false, "Please try again." ] ]
+  },
 
-    "<br><br><b>2.</b> ?<br>",
-	{
-		questionType : "multiple choice",
-		choices : [
-				["", false, "Please try again." ],
-				["", false, "Please try again." ],
-				["", true, "Correct!" ],
-				["", false, "Please try again." ] ]
-	},
+    "<br><br><b>2.</b> For delete we used the method indexOf to find the index of the movie quote in the array.  Why didn't we just use an old school for loop that uses an index?<br>",
+  {
+    questionType : "multiple choice",
+    choices : [
+        ["Swift doesn't have a traditional for loop that uses an index.  You have to use an enhanced for loop (which has no index).", false, "Please try again." ],
+        ["I'm lazy.  I copied the code from .ChildChanged which used an enhanced for loop style with no index, so that resulted in needing to use indexOf. Another approach would easily avoid the necessity of calling indexOf", true, "Correct!  If you wanted, could you refactor the code easily to avoid using indexOf? :)" ] ]
+  }
 
 ];
