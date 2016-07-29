30e3cba3953cac1104d6649ece15d5a3b3f94d11
David Fisher <fisherds@rose-hulman.edu>
2016-07-29 16:29:16 -0400

    Adding the last two follow along videos for iOS Firebase auth

--- appengine/courses/ios-firebase/assets/js/activity-2.5.js
+++ appengine/courses/ios-firebase/assets/js/activity-2.5.js
@@ -1,23 +1,34 @@
 var activity = [
 
-    "<b>1.</b> ?<br>",
-	{
-		questionType : "multiple choice",
-		choices : [
-				["", false, "Please try again." ],
-				["", false, "Please try again." ],
-				["", true, "Correct!" ],
-				["", false, "Please try again." ] ]
-	},
+    "<b>1.</b> Why do we make the <b>key</b> field optional in the Password object? Why use <b>var key: String?</b> instead of just <b>var key: String!</b><br>",
+  {
+    questionType : "multiple choice",
+    choices : [
+        ["Not all objects that are stored in the Firebase Database have a key", false, "Please try again." ],
+        ["When we display data in our table we don't need a key for all the Passwords", false, "Please try again." ],
+        ["For that 0.0001 millisecond when we make a Password before pushing it to Firebase we don't have the key field set.  All other times there is a key on there.", true, "Correct!" ] ]
+  },
 
-    "<br><br><b>2.</b> ?<br>",
-	{
-		questionType : "multiple choice",
-		choices : [
-				["", false, "Please try again." ],
-				["", false, "Please try again." ],
-				["", true, "Correct!" ],
-				["", false, "Please try again." ] ]
-	},
+    "<br><br><b>2.</b> In Android development and web development Firebase uses the verb <b>push</b> to get an auto generated key and add a new object to the Database.  In iOS development what is the name of the method used to <b>push</b> a new Password to the Database?<br>",
+
+    {
+      questionType : "freetext",
+      correctAnswerRegex : /childByAutoId/i,
+      correctAnswerOutput : "Correct! childByAutoId()",
+      incorrectAnswerOutput : "Please try again.",
+      showAnswerOutput : "Here is the answer: childByAutoId()"
+    },
+
+
+    "<br><br><b>3.</b> At this moment we have CRUD working with Anonymous Auth.  When we implement Email/Password Auth, Rosefire Auth, and Google Auth will the CRUD methods change?<br>",
+
+
+    {
+      questionType : "multiple choice",
+      choices : [
+          ["They will change a lot", false, "Please try again." ],
+          ["They will change a little but not much", false, "Please try again." ],
+          ["They will not change.  Doesn't matter how someone signs in", true, "Correct! That's really cool btw. Now when a new auth method works it'll just work all the way through too." ] ]
+    },
 
 ];
--- appengine/courses/ios-firebase/assets/js/activity-2.6.js
+++ appengine/courses/ios-firebase/assets/js/activity-2.6.js
@@ -1,23 +1,28 @@
 var activity = [
 
-    "<b>1.</b> ?<br>",
-	{
-		questionType : "multiple choice",
-		choices : [
-				["", false, "Please try again." ],
-				["", false, "Please try again." ],
-				["", true, "Correct!" ],
-				["", false, "Please try again." ] ]
-	},
+    "<b>1.</b> When a developer uses only the Email / Password auth approach does the user need to create a custom password for this app?<br>",
+    {
+      questionType : "multiple choice",
+      choices : [
+          [ "Yep.  Yet another email/password combo for the user to have to remember.", true, "Correct! Can you tell I don't like that approach? :)" ],
+          [ "The answer above it correct.  Select it. :)", false, "Please try again." ] ]
+    },
 
-    "<br><br><b>2.</b> ?<br>",
-	{
-		questionType : "multiple choice",
-		choices : [
-				["", false, "Please try again." ],
-				["", false, "Please try again." ],
-				["", true, "Correct!" ],
-				["", false, "Please try again." ] ]
-	},
+    "<br><br><b>2.</b> In this unit I had two users, they were a@b.com and president@us.gov.  Is there a way to prevent that kind of nonsense in a real app?<br>",
+    {
+      questionType : "multiple choice",
+      choices : [
+          [ "Yep.  You just need to send them emailVerification and make sure the emailVerified boolean is true before authorizing them to enter.",
+              true, "Correct!" ], [ "No way to control what email a user enters", false, "Please try again." ] ]
+    },
+
+    "<br><br><b>3.</b> Which of these methods was used for the <b>Sign up</b> button?<br>",
+    {
+      questionType : "multiple choice",
+      choices : [ [ "FIRAuth.auth()?.createUserWithEmail(password:completion:)", true, "Correct!" ],
+          [ "FIRAuth.auth()?.signInWithEmail(password:completion:)", false, "Please try again." ],
+          [ "FIRApp.createUserWithEmail(password:completion:)", false, "Please try again." ],
+          [ "FIRApp.signInWithEmail(password:completion:)", false, "Please try again." ] ]
+    },
 
 ];
