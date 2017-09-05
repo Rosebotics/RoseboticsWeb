a7e349f7656e910a7b91a997ba1a91fdde8245a3
David Fisher <fisherds@rose-hulman.edu>
2017-09-05 15:07:08 -0400

    porting The Accumulator Pattern

---
+++ appengine/courses/csse120-intro/assets/js/activity-4.1.js
@@ -0,0 +1,43 @@
+var activity = [
+
+  "<b>1.</b> Consider the following code.<br>",
+  "<img src='assets/img/conditionals-1.png' alt = 'conditional-1' > ",
+  "<p>What statement should be added in the box so the function prints <code> Banana! </code> only if the value of <code>n</code> is larger than 20? </p> <br>",
+  {
+    questionType: "multiple choice",
+    choices: [
+      ["<code> if n < 20: </code>", false, "Please try again."],
+      ["<code> if n > 20: </code>", true, "Correct!"],
+      ["<code> elif n > 20: </code>", false, "Please try again."],
+      ["<code> else n > 20: </code>", false, "Please try again."],
+    ]
+  },
+  "<br><br>For the following questions, consider Versions 1 and 2:<br>",
+  "<img src='assets/img/conditionals-3.png' alt='conditionals-3'>",
+  "<br><br><b>2.</b> Versions 1 and 2 will print the same thing given the same <code>x</code> and <code>y</code>. <br>",
+  {
+    questionType: "multiple choice",
+    choices: [
+      ["True", true, "Correct!"],
+      ["False", false, "Please try again."],
+    ]
+  },
+  "<br><br><b>3.</b> Which of the versions runs faster? <br>",
+  {
+    questionType: "multiple choice",
+    choices: [
+      ["Version 1", true, "Correct!"],
+      ["Version 2", false, "Please try again."],
+      ["Neither (they run equally fast)", false, "Please try again."],
+    ]
+  },
+  "<br><br><b>4.</b> Which of the versions is better? <br>",
+  {
+    questionType: "multiple choice",
+    choices: [
+      ["Version 1", true, "Correct!"],
+      ["Version 2", false, "Please try again."],
+      ["Neither (they are equally good)", false, "Please try again."],
+    ]
+  }
+];
