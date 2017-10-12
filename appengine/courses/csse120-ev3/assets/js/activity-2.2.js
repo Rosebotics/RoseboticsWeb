e74dee03b822b8a4088e3fe08a58b195d8ecb2ff
David Fisher <fisherds@rose-hulman.edu>
2017-10-12 10:24:57 -0400

    questions for Digital Inputs

--- appengine/courses/csse120-ev3/assets/js/activity-2.2.js
+++ appengine/courses/csse120-ev3/assets/js/activity-2.2.js
@@ -1,23 +1,216 @@
 var activity = [
 
-    "<b>1.</b> ?<br>",
+	"When we first discussed callback functions we made simple callbacks like this:<br>",
+	"<pre>def print_hello(state):<br>    if state:<br>        print('hello')<br><br><br>btn = ev3.Button()<br>btn.on_down = print_hello</pre>",
+	"<b>1.</b> What is the problem with that style of simple callback?<br>",
+    {
+      questionType : "multiple choice",
+      choices : [
+          ["It doesn't work", false, "Please try again." ],
+          ["You can only use 1 line of code", false, "Please try again." ],
+          ["You can't pass parameters to the callback other than state", true, "Correct!" ],
+          ["There are no parameters at all", false, "Please try again." ] ]
+    },
+
+	"<br><br>Consider the code templates below<br>",
+	"<pre>keyword name(arguments):<br>    print('Can have')<br>    print('many statements')</pre>",
+	"<pre>name = keyword arguments:<br>    print('Can have only 1 statement')</pre>",
+
+	"<b>2.</b> What is the keyword in code snippet 1 to make a function?<br>",
 	{
-		questionType : "multiple choice",
-		choices : [
-				["", false, "Please try again." ],
-				["", false, "Please try again." ],
-				["", true, "Correct!" ],
-				["", false, "Please try again." ] ]
+		questionType: "freetext",
+		correctAnswerRegex: /def/i,
+		correctAnswerOutput: "Correct!",
+		incorrectAnswerOutput: "Please try again.",
 	},
 
-    "<br><br><b>2.</b> ?<br>",
+	"<br><br><b>3.</b> What is the keyword in code snippet 2 to make a function?<br>",
 	{
-		questionType : "multiple choice",
-		choices : [
-				["", false, "Please try again." ],
-				["", false, "Please try again." ],
-				["", true, "Correct!" ],
-				["", false, "Please try again." ] ]
+		questionType: "freetext",
+		correctAnswerRegex: /lambda/i,
+		correctAnswerOutput: "Correct!",
+		incorrectAnswerOutput: "Please try again.",
 	},
+
+	"<br><br>Consider the following code<br>",
+	"<pre>def print_multiples(state, my_str, n):<br>    if state:<br>        for k in range(n):<br>            print(my_str)<br><br><br>btn = ev3.Button()<br>btn.on_down = lambda state: print_multiples(state, 'Dave', 3)</pre>",
+	"<b>4.</b> When you press the down button what all happens?<br>",
+	{
+		questionType: "multiple choice group",
+		questionsList: [{
+				questionHTML: "<b>a.</b> What happens first ",
+				choices: ["The print_multiples function prints 'Dave' 3 times, one 'Dave' per line",
+					"The print_multiples function runs but prints nothing",
+					"The lambda function calls the def function print_multiples, passing in False, 'Dave', and 3",
+					"The lambda function calls the def function print_multiples, passing in True, 'Dave', and 3",
+					"Under the hood the system calls btn.on_down(True)",
+					"Under the hood the system calls btn.on_down(False)"
+				],
+				correctIndex: 4
+			},
+			{
+				questionHTML: "<b>b.</b> What happens second",
+				choices: ["The print_multiples function prints 'Dave' 3 times, one 'Dave' per line",
+					"The print_multiples function runs but prints nothing",
+					"The lambda function calls the def function print_multiples, passing in False, 'Dave', and 3",
+					"The lambda function calls the def function print_multiples, passing in True, 'Dave', and 3",
+					"Under the hood the system calls btn.on_down(True)",
+					"Under the hood the system calls btn.on_down(False)"
+				],
+				correctIndex: 3
+			},
+			{
+				questionHTML: "<b>c.</b> What happens thrid",
+				choices: ["The print_multiples function prints 'Dave' 3 times, one 'Dave' per line",
+					"The print_multiples function runs but prints nothing",
+					"The lambda function calls the def function print_multiples, passing in False, 'Dave', and 3",
+					"The lambda function calls the def function print_multiples, passing in True, 'Dave', and 3",
+					"Under the hood the system calls btn.on_down(True)",
+					"Under the hood the system calls btn.on_down(False)"
+				],
+				correctIndex: 0
+			}
+		],
+		allCorrectOutput: "Well done!",
+		someIncorrectOutput: "Please try again.",
+	},
+	"<br><br><b>5.</b> When the down button is released what will happen?<br>",
+
+	{
+		questionType: "multiple choice group",
+		questionsList: [{
+				questionHTML: "<b>a.</b> What happens first ",
+				choices: ["The print_multiples function prints 'Dave' 3 times, one 'Dave' per line",
+					"The print_multiples function runs but prints nothing",
+					"The lambda function calls the def function print_multiples, passing in False, 'Dave', and 3",
+					"The lambda function calls the def function print_multiples, passing in True, 'Dave', and 3",
+					"Under the hood the system calls btn.on_down(True)",
+					"Under the hood the system calls btn.on_down(False)"
+				],
+				correctIndex: 5
+			},
+			{
+				questionHTML: "<b>b.</b> What happens second",
+				choices: ["The print_multiples function prints 'Dave' 3 times, one 'Dave' per line",
+					"The print_multiples function runs but prints nothing",
+					"The lambda function calls the def function print_multiples, passing in False, 'Dave', and 3",
+					"The lambda function calls the def function print_multiples, passing in True, 'Dave', and 3",
+					"Under the hood the system calls btn.on_down(True)",
+					"Under the hood the system calls btn.on_down(False)"
+				],
+				correctIndex: 2
+			},
+			{
+				questionHTML: "<b>c.</b> What happens thrid",
+				choices: ["The print_multiples function prints 'Dave' 3 times, one 'Dave' per line",
+					"The print_multiples function runs but prints nothing",
+					"The lambda function calls the def function print_multiples, passing in False, 'Dave', and 3",
+					"The lambda function calls the def function print_multiples, passing in True, 'Dave', and 3",
+					"Under the hood the system calls btn.on_down(True)",
+					"Under the hood the system calls btn.on_down(False)"
+				],
+				correctIndex: 1
+			}
+		],
+		allCorrectOutput: "Well done!",
+		someIncorrectOutput: "Please try again.",
+	},
+
+	"<br><br>Categorize each name below as either an instance variable of type _______ or as a method?<br>",
+	"Then select which class it belongs to <b>ev3.Button</b>, <b>ev3.TouchSensor</b>, or <b>ev3.RemoteControl</b>.<br>",
+	"Then select if that name is used with States or Events<br><br>",
+
+	"<b>6.</b> on_blue_up<br>",
+	{
+		questionType: "multiple choice group",
+		questionsList: [{
+				questionHTML: "<b>a.</b> <b>on_blue_up</b> is the name for an...",
+				choices: ["Instance variable of type bool", "Instance variable of type function", "method"],
+				correctIndex: 1
+			},
+			{
+				questionHTML: "<b>b.</b> <b>on_blue_up</b> is part of which class?",
+				choices: ["ev3.Button", "ev3.TouchSensor", "ev3.RemoteControl"],
+				correctIndex: 2
+			},
+			{
+				questionHTML: "<b>c.</b> <b>on_blue_up</b> is used when...",
+				choices: ["working with States", "working with Events"],
+				correctIndex: 1
+			},
+		],
+		allCorrectOutput: "Well done!",
+		someIncorrectOutput: "Please try again.",
+	},
+
+	"<br><br><b>7.</b> process<br>",
+	{
+		questionType: "multiple choice group",
+		questionsList: [{
+				questionHTML: "<b>a.</b> <b>process</b> is the name of a...",
+				choices: ["Instance variable of type bool", "Instance variable of type function", "method"],
+				correctIndex: 2
+			},
+			{
+				questionHTML: "<b>b.</b> <b>process</b> is part of which class(es)?",
+				choices: ["ev3.Button", "ev3.TouchSensor", "ev3.RemoteControl"],
+				correctIndex: [0, 2]
+			},
+			{
+				questionHTML: "<b>c.</b> <b>process</b> is used when...",
+				choices: ["working with States", "working with Events"],
+				correctIndex: 1
+			},
+		],
+		allCorrectOutput: "Well done!",
+		someIncorrectOutput: "Please try again.",
+	},
+
+	"<br><br><b>8.</b> is_pressed<br>",
+	{
+		questionType: "multiple choice group",
+		questionsList: [{
+				questionHTML: "<b>a.</b> <b>is_pressed</b> is the name for an...",
+				choices: ["Instance variable of type bool", "Instance variable of type function", "method"],
+				correctIndex: 0
+			},
+			{
+				questionHTML: "<b>b.</b> <b>is_pressed</b> is part of which class?",
+				choices: ["ev3.Button", "ev3.TouchSensor", "ev3.RemoteControl"],
+				correctIndex: 1
+			},
+			{
+				questionHTML: "<b>c.</b> <b>is_pressed</b> is used when...",
+				choices: ["working with States", "working with Events"],
+				correctIndex: 0
+			},
+		],
+		allCorrectOutput: "Well done!",
+		someIncorrectOutput: "Please try again.",
+	},
+
+	"<br><br><b>9.</b> With our robot how many instances would you make (max) for each of the following classes?<br>",
+
+    {
+		questionType : "multiple choice group",
+		questionsList : [
+			{
+			  questionHTML : "<b>a.</b> ev3.Button",
+			  choices : [ "0", "1", "4", "As many as you want!" ],
+			  correctIndex : 1
+			},
+			{
+			  questionHTML : "<b>a.</b> ev3.TouchSensor",
+			  choices : [ "0", "1", "4", "As many as you want!" ],
+			  correctIndex : 1
+			},
+			{
+			  questionHTML : "<b>a.</b> ev3.RemoteControl",
+			  choices : [ "0", "1", "4", "As many as you want!" ],
+			  correctIndex : 2
+			},],
+		allCorrectOutput : "Well done!",
+		someIncorrectOutput : "Please try again.",
+	  },
 
 ];
--- appengine/courses/csse120-ev3/assets/js/activity-2.3.js
+++ appengine/courses/csse120-ev3/assets/js/activity-2.3.js
@@ -1,23 +1,86 @@
 var activity = [
 
-    "<b>1.</b> ?<br>",
+	"Consider this code snippet:",
+	"<pre>ev3.Leds.set_color(__________, ____________)</pre>",
+	"<b>1.</b> Fill in the blanks.<br>",
 	{
-		questionType : "multiple choice",
-		choices : [
-				["", false, "Please try again." ],
-				["", false, "Please try again." ],
-				["", true, "Correct!" ],
-				["", false, "Please try again." ] ]
+		questionType: "multiple choice group",
+		questionsList: [{
+				questionHTML: "<b>a.</b> What values might go into blank 1",
+				choices: ["ev3.Leds.LEFT", "ev3.Leds.BLACK", "ev3.Leds.RED", "ev3.Leds.RIGHT", "ev3.Leds.BLUE", "ev3.Leds.AMBER", "ev3.Leds.GREEN", ],
+				correctIndex: [0, 3]
+			},
+			{
+				questionHTML: "<b>a.</b> What values might go into blank 2",
+				choices: ["ev3.Leds.LEFT", "ev3.Leds.BLACK", "ev3.Leds.RED", "ev3.Leds.RIGHT", "ev3.Leds.BLUE", "ev3.Leds.AMBER", "ev3.Leds.GREEN", ],
+				correctIndex: [1, 2, 5, 6]
+			}
+		],
+		allCorrectOutput: "Well done!",
+		someIncorrectOutput: "Please try again. Hints: The LEDs are red/green LEDs.  No combination of red and green make BLUE",
 	},
 
-    "<br><br><b>2.</b> ?<br>",
+	"<br><br>Consider the following poorly written code snippet:",
+	"<pre>ev3.Sound.tone(1000, 5).wait()</pre>",
+	"<b>2.</b> What frequency would this command try to play?<br>",
 	{
-		questionType : "multiple choice",
-		choices : [
-				["", false, "Please try again." ],
-				["", false, "Please try again." ],
-				["", true, "Correct!" ],
-				["", false, "Please try again." ] ]
+		questionType: "multiple choice",
+		choices: [
+			["1 Hz", false, "Please try again."],
+			["5 Hz", false, "Please try again."],
+			["1000 Hz", true, "Correct!"],
+			["5000 Hz", false, "Please try again."]
+		]
 	},
+	"<br><br><b>3.</b> For how long would the robot play that frequency?<br>",
+	{
+		questionType: "multiple choice",
+		choices: [
+			["5 millisecond", true, "Correct!"],
+			["1000 milliseconds", false, "Please try again."],
+			["5 seconds", false, "Please try again."],
+			["1000 seconds", false, "Please try again."],
+			["5000 seconds", false, "Please try again."]
+		]
+	},
+	"<br><br><b>4.</b> Write the line of code needed to play 500 Hz for 1 second.<br>",
+	{
+		questionType: "freetext",
+		correctAnswerRegex: /ev3.Sound.tone\(500\,\s*1000\)/,
+		correctAnswerOutput: "Correct!",
+		incorrectAnswerOutput: "Please try again.",
+		showAnswerOutput: "Here is the answer: ev3.Sound.tone(500, 1000)"
+	},
+	"<br><br><b>5.</b> When using the ev3.Screen it's common to temporarially turn off the Brickman program.  Why?<br>",
+	{
+		questionType: "multiple choice",
+		choices: [
+			["When Brickman is running there is no way to draw to the screen", false, "Please try again."],
+			["You wouldn't ever turn off Brickman", false, "Please try again."],
+			["Brickman will refresh the screen every 1 second or so with the Brickman UI, so it's annoying", true, "Correct!"],
+		]
+	},
+	"<br><br><b>6.</b> What command do you type into the SSH terminal to temporarially stop the Brickman program<br>",
+	{
+		questionType: "freetext",
+		correctAnswerRegex: /sudo chvt 6/,
+		correctAnswerOutput: "Correct!",
+		incorrectAnswerOutput: "Please try again.",
+	},
+	"<br><br><b>7.</b> What command do you type into the SSH terminal to restart the Brickman program<br>",
+	{
+		questionType: "freetext",
+		correctAnswerRegex: /sudo chvt 1/,
+		correctAnswerOutput: "Correct!",
+		incorrectAnswerOutput: "Please try again.",
+	},
+	"<br><br><b>8.</b> What is the password to use sudo on our robots?<br>",
+	{
+		questionType: "freetext",
+		correctAnswerRegex: /^C\$\$E120$/,
+		correctAnswerOutput: "Correct!",
+		incorrectAnswerOutput: "Please try again.",
+	},
+
 
 ];
