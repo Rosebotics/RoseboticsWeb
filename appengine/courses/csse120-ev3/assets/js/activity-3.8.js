12bf968daceaea6e88db8439e0406b1bfdd9a931
David Fisher <fisherds@rose-hulman.edu>
2017-10-27 22:42:50 -0400

    minor updates when watch the videos

--- appengine/courses/csse120-ev3/assets/js/activity-3.8.js
+++ appengine/courses/csse120-ev3/assets/js/activity-3.8.js
@@ -32,7 +32,7 @@ var activity = [
 	{
 		questionType: "multiple choice",
 		choices: [
-			["Nope, not yet, still set as 99.  One person on our team will change it when we have the robot connected, commit that change, transfer the change to the EV3, then everyone else will need to git update.", true, "Correct! So be sure you understand the MQTT modules"],
+			["Nope, not yet, still set as 99.  One person on our team will change it when we have the robot connected, commit that change, transfer the change to the EV3, then everyone else will need to git update.", true, "Correct!  Great that will work out best."],
 			["Yep it has been changed already!", false, "Please try again.  If you DID do it, then make sure you upload that file to your EV3, so that the file is up to date on your EV3. (now mark the other answer to get the question right)"]
 		]
 	},
