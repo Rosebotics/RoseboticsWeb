293d94019cca9fdea66b8621b81d10a7989b6698
David Fisher <fisherds@rose-hulman.edu>
2018-02-18 22:31:55 -0500

    Minor question updates

--- appengine/courses/ios-swift-intro/assets/js/activity-2.2.js
+++ appengine/courses/ios-swift-intro/assets/js/activity-2.2.js
@@ -1,21 +1,21 @@
 var activity = [
 
     "<b>1.</b> In the first video we used the pin view icon to add constraints.  In this video we used right click drag to add constraints.  Which do you prefer?<br>",
-	{
-		questionType : "multiple choice",
-		choices : [
-				["I like the pin menu better because it's easier to add multiple constraints at a time (it's quick!)", false, "Please try again."  ],
-				["I like the right click drag better because it's easier to add complex arrangements like center Y (it's easy to specify what the two views are in the constraint instead of forcing you to use only the nearest neighbor", false, "Please try again."  ],
-				["All of the above", true, "Correct! There is no correct answer, but are useful." ],
-				["None of the above", false, "Please try again." ] ]
-	},
+  {
+    questionType : "multiple choice",
+    choices : [
+        ["I like the pin menu better because it's easier to add multiple constraints at a time (it's quick!)", false, "Please try again."  ],
+        ["I like the right click drag better because it's easier to add complex arrangements like center Y (it's easy to specify what the two views are in the constraint instead of forcing you to use only the nearest neighbor", false, "Please try again."  ],
+        ["All of the above", true, "Correct! There is no correct answer, both are useful." ],
+        ["None of the above", false, "Please try again." ] ]
+  },
 
     "<br><br><b>2.</b> If you want to see what your app will look like on different sizes what is the fastest way to do that?<br>",
-	{
-		questionType : "multiple choice",
-		choices : [
-				["Run it on the simulator", false, "Please try again." ],
-				["Use the Assistant Editor Preview window for different devices and orientations", true, "Correct! Love that window (assuming you have a big screen to work on)" ] ]
-	},
+  {
+    questionType : "multiple choice",
+    choices : [
+        ["Run it on the simulator", false, "Please try again." ],
+        ["Use the Assistant Editor Preview window for different devices and orientations", true, "Correct! Love that window (assuming you have a big screen to work on)" ] ]
+  },
 
 ];
